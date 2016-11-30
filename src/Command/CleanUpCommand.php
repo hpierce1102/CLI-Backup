5096e5b6629e3c0717b5b79b680bf18d129898d0
hpierce <hpierce1102@gmail.com>
2016-11-29 20:01:32 -0600

    Implemented the command that deletes old backups.

--- src/Command/CleanUpCommand.php
+++ src/Command/CleanUpCommand.php
@@ -8,6 +8,9 @@
 
 namespace Backup\Command;
 
+use Backup\Uploader\UploaderInterface;
+use Backup\User\UserInterface;
+use Backup\Util\ConfigParser;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -29,6 +32,60 @@ class CleanUpCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $storageEngine = ConfigParser::getStorageEngine();
 
+        $userAlias = $input->getArgument('userAlias');
+
+        /** @var UserInterface $user */
+        $user = $storageEngine->retrieveUser($userAlias);
+
+
+        $uploaderClass = $user->getUploaderClass();
+
+        /** @var UploaderInterface $uploader */
+        $uploader = new $uploaderClass($user);
+        $uploader->setOutput($output);
+
+        $files = $uploader->listFiles('/');
+
+        $maxAge = \DateInterval::createFromDateString($input->getArgument('age'));
+
+        $maxAgeSeconds = $this->convertDateIntervalToSeconds($maxAge);
+        $now = new \DateTime('now');
+
+        $files = array_filter($files, function($file) use ($now, $maxAgeSeconds){
+            /** @var \DateTime $date */
+            $date = $file['date'];
+            $seconds = $now->getTimestamp() - $date->getTimestamp();
+            return $seconds > $maxAgeSeconds;
+        });
+
+        $files = array_map(function($file){
+            return $file['location'];
+        }, $files);
+
+        if(!empty($files)){
+            $fileCount = count($files);
+            $output->writeln(sprintf('Deleting %s files.', $fileCount));
+            $uploader->purgeFiles($files);
+            $output->writeln(sprintf('Deleted %s files successfully.', $fileCount));
+        } else {
+            $output->writeln('No files were old enough to delete.');
+        }
+    }
+
+    private function convertDateIntervalToSeconds(\DateInterval$maxAge){
+        $seconds = $maxAge->s;
+        $seconds += ($maxAge->i * 60);
+        $seconds += ($maxAge->h * 3600);
+        $seconds += ($maxAge->d * 86400);
+        //This is calculated conservatively as 31 days because "1 month" is acceptable input from PHP's standpoint
+        //but is quite ambiguous when trying to convert it to seconds as some months have 31 days and February
+        //has only 28 days. We use the 31 days because that will result in less purged files, which is less damaging
+        //than accidentally deleting files that *weren't* supposed to be deleted.
+        $seconds += ($maxAge->m * 2678400);
+        $seconds += ($maxAge->y * 31536000);
+
+        return $seconds;
     }
 }
