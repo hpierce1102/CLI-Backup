07a0e28d54b8c2d0e7ea4af3b4d9f0963e8151dd
hpierce <hpierce1102@gmail.com>
2016-12-01 19:49:43 -0600

    Implemented DebugCommand to display users accross all StorageEngines.

--- src/Command/DebugCommand.php
+++ src/Command/DebugCommand.php
@@ -8,6 +8,9 @@
 
 namespace Backup\Command;
 
+use Backup\StorageEngine\StorageEngineInterface;
+use Backup\Util\ClassFinder;
+use Backup\Util\ConfigParser;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -23,14 +26,48 @@ class DebugCommand extends Command
             ->setDescription('Displays the state of the application.')
             ->setHelp("Displays the state of the application.")
 
-            //Choices: Users or StorageEngines
-            ->addArgument('entity', InputArgument::REQUIRED, "Which entity do you need to check?")
             ->addOption('json', null, InputOption::VALUE_NONE , 'Run with this option to get json output only.');
         ;
     }
     
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $config = ConfigParser::getConfig();
+
+        $users = new \stdClass();
+        foreach(ClassFinder::getClassesInNamespace('Backup\\StorageEngine', 'Backup\\StorageEngine\\StorageEngineInterface') as $class){
+
+            /** @var StorageEngineInterface $storageEngine */
+            $storageEngine = $class::initFromConfig($config);
+
+            $users->$class = $storageEngine->listUsers();
+        }
+
+        if($input->getOption('json')){
+            $json = json_encode($users);
+            $output->write($json);
+        } else {
+            $storageEngines = (array) $users;
+
+            foreach($storageEngines as $key => $storageEngineUsers){
+                $output->writeln(sprintf('Showing users stored in: %s' ,$key));
+
+                if(empty($storageEngineUsers)){
+                    $output->writeln('  NONE');
+                    continue;
+                }
+
+                foreach($storageEngineUsers as $user){
+                    $output->writeln('  ' . $user['alias']);
+                    $userParams = (array) $user['user'];
+
+                    foreach($userParams as $paramKey => $paramValue){
+                        $output->writeln(sprintf("    %s : %s", $paramKey, $paramValue));
+                    }
+                }
+            }
+        }
+
 
     }
 }
