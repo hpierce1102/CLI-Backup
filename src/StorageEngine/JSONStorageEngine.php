f4968ca9d2e43175506b18c42a21649c6d81e509
hpierce <hpierce1102@gmail.com>
2016-12-08 19:48:18 -0600

    Implemented JSON StorageEngine

--- src/StorageEngine/JSONStorageEngine.php
+++ src/StorageEngine/JSONStorageEngine.php
@@ -8,10 +8,28 @@
 
 namespace Backup\StorageEngine;
 
+use Backup\Exception\JSONException;
 use Backup\User\User;
 
 class JSONStorageEngine implements StorageEngineInterface
 {
+    protected $path;
+    protected $users;
+
+    public function __construct($path)
+    {
+        if(!file_exists($path)){
+            file_put_contents($path, json_encode( [] ));
+        }
+
+        $this->path = $path;
+        $this->users = json_decode(file_get_contents($path));
+
+        if(JSON_ERROR_NONE !== json_last_error()){
+            throw new JSONException(json_last_error_msg());
+        }
+    }
+
     public static function getName()
     {
         return "JSON";
@@ -19,21 +37,51 @@ class JSONStorageEngine implements StorageEngineInterface
 
     public static function initFromConfig(Array $config)
     {
-        return new self();
+        if(!isset($config['StorageEngine']['JSON']['file'])){
+            throw new InvalidConfigStateException('Could not read file option in Config:StorageEngine:JSON:file');
+        } else {
+            return new static($config['StorageEngine']['JSON']['file']);
+        }
+
     }
     
     public function persistUser(String $userAlias, User $user)
     {
-        // TODO: Implement persistUser() method.
+        $record = new \stdClass();
+
+        $record->alias = $userAlias;
+        $record->params = serialize($user);
+
+        $this->users[] = $record;
+
+        file_put_contents($this->path, json_encode($this->users));
     }
 
     public function retrieveUser(String $userAlias)
     {
-        // TODO: Implement retrieveUser() method.
+        foreach($this->users as $user){
+            if($user->alias == $userAlias){
+                $retrievedUser = $user;
+                break;
+            }
+        }
+
+        if(isset($retrievedUser)){
+            $retrievedUser = unserialize($retrievedUser->params);
+
+            return $retrievedUser;
+        } else {
+            return null;
+        }
+
     }
 
     public function listUsers()
     {
-        return [];
+        return array_map(function($user){
+            $user->id = $user->alias;
+            unset($user->alias);
+            return (array) $user;
+        },$this->users);
     }
 }
