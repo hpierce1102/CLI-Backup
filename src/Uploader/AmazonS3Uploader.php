63bbc2dfc65229762bccfa147d90fa6d858874a9
Hayden Pierce <hayden@haydenpierce.com>
2016-12-18 19:34:19 -0600

    S3Uploader will now trim off the prefixing /'s to prevent an unneeded extra directory. Listfiles() actually works now and will provide more than one record as needed. It also accepts prefixes as dictated by the interface.

--- src/Uploader/AmazonS3Uploader.php
+++ src/Uploader/AmazonS3Uploader.php
@@ -55,6 +55,8 @@ class AmazonS3Uploader implements UploaderInterface
 
     public function publishFile(String $filePath, String $location)
     {
+        $location = trim($location, '/');
+
         $result = $this->s3->putObject(array(
             'Bucket'     => $this->user->getBucket(),
             'Key'        => $location,
@@ -67,6 +69,8 @@ class AmazonS3Uploader implements UploaderInterface
 
     public function purgeFile(String $location)
     {
+        $location = trim($location, '/');
+
         $this->s3->deleteObject(array(
             'Bucket' => $this->user->getBucket(),
             'Key' => $location
@@ -80,16 +84,28 @@ class AmazonS3Uploader implements UploaderInterface
         }
     }
 
-    public function listFiles(String $filePath)
+    public function listFiles(String $location)
     {
-        $objects = $this->s3->getIterator('ListObjects', array(
+        $location = ltrim($location, '/');
+
+        $options = array(
             'Bucket' => $this->user->getBucket()
-        ));
+        );
+
+        if(!empty($location)){
+            $options['Prefix'] = $location;
+        }
+
+        $objects = $this->s3->getIterator('ListObjects', $options);
 
         $files = [];
+        var_dump($objects);
         foreach($objects as $object){
-            $files['location'] = $object['Key'];
-            $files['date'] = $object['LastModified']; //This object extends DateTime.
+            $objectArray = array(
+                'location' => $object['Key'],
+                'date' => $object['LastModified'] //This object extends DateTime.
+            );
+            $files[] = $objectArray;
         }
 
         return $files;
