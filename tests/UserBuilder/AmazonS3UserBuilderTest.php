14c8336d4aefcdfa66eea6a9da7bdf56d2d15a09
Hayden Pierce <hayden@haydenpierce.com>
2017-01-28 05:23:57 -0600

    Added unit test for AmazonS3UserBuilder.

---
+++ tests/UserBuilder/AmazonS3UserBuilderTest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @copyright Hayden Pierce (hayden@haydenpierce.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Backup\Tests\Util;
+
+use Backup\User\AmazonS3User;
+use Backup\UserBuilder\AmazonS3UserBuilder;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Output\NullOutput;
+
+class AmazonS3UserBuilderTest extends TestCase
+{
+    public function testCreatesUser()
+    {
+        $builder = new AmazonS3UserBuilder();
+
+        $output = new NullOutput();
+        $actualUser = $builder->buildUser($output);
+
+        $expectedUser = new AmazonS3User();
+        $expectedUser->setBucket('Placeholder');
+        $expectedUser->setProfile('Placeholder');
+        $expectedUser->setRegion('Placeholder');
+
+        $this->assertEquals($expectedUser, $actualUser);
+    }
+}
