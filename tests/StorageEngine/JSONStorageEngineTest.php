8a3aef2ee72845a48d752c6d3d8eb3fc544d289f
Hayden Pierce <hayden@haydenpierce.com>
2017-01-28 10:41:22 -0600

    Added JSONStorageEngine unit test.

---
+++ tests/StorageEngine/JSONStorageEngineTest.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * @copyright Hayden Pierce (hayden@haydenpierce.com)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Backup\Tests\StorageEngine;
+
+use Backup\StorageEngine\JSONStorageEngine;
+use Backup\StorageEngine\SQLiteStorageEngine;
+use Backup\User\AmazonS3User;
+use PHPUnit\Framework\TestCase;
+
+class JSONStorageEngineTest extends TestCase
+{
+    /** @var JSONStorageEngine */
+    protected $storageEngine;
+
+    /** @var  AmazonS3User */
+    protected $user;
+
+    /** @var  String */
+    protected $alias;
+
+    public function setup()
+    {
+        $this->user = new AmazonS3User();
+        $this->alias = 'user';
+    }
+
+    //Ensures that there doesn't happen to be a file with a similar name to the test DB that would get deleted.
+    //Seems unlikely - but better safe than sorry.
+    public static function setUpBeforeClass()
+    {
+        $files = scandir(__DIR__);
+
+        foreach($files as $file){
+            if(strpos($file, 'TempJSONTest') === 0){
+                throw new \Exception(sprintf("A file \"%s/%s\" exists. Running tests will delete files like this.
+In order to prevent unwanted desturction, delete this file before running the tests.",
+                    __DIR__,
+                    $file));
+            }
+        }
+    }
+
+    public static function tearDownAfterClass()
+    {
+        $files = scandir(__DIR__);
+
+        foreach($files as $file){
+            if(strpos($file, 'TempJSONTest') === 0){
+                unlink(__DIR__ .'/' . $file);
+            }
+        }
+    }
+
+    public function testPersistUser()
+    {
+        $tempDB = tempnam(__DIR__, 'TempJSONTest');
+        $storageEngine = new JSONStorageEngine($tempDB);
+
+        $true = $storageEngine->persistUser($this->alias, $this->user);
+        $this->assertTrue($true, 'StorageEngineInterface::persistUser() should return boolean.');
+
+        return $storageEngine;
+    }
+
+    /**
+     * @param SQLiteStorageEngine $storageEngine
+     * @depends testPersistUser
+     */
+    public function testRetrieveUser($storageEngine)
+    {
+        $actualUser = $storageEngine->retrieveUser($this->alias);
+        $this->assertEquals($this->user, $actualUser);
+    }
+
+    /**
+     * @param SQLiteStorageEngine $storageEngine
+     * @depends testPersistUser
+     */
+    public function testListUsers($storageEngine)
+    {
+        $actualUsers = $storageEngine->listUsers();
+        $expectedUsers = [
+            [
+                'alias' => $this->alias,
+                'user' => $this->user
+            ]
+        ];
+
+        $this->assertEquals($expectedUsers, $actualUsers);
+    }
+
+    /**
+     * @param SQLiteStorageEngine $storageEngine
+     * @depends testPersistUser
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage User user already exists in the JSON. Cannot add this user.
+     */
+    public function testDuplicatePersistUser($storageEngine)
+    {
+        $storageEngine->persistUser($this->alias, $this->user);
+    }
+
+    public function testNonExistentFileWorks()
+    {
+        $name = tempnam(__DIR__, 'TempJSONTest');
+        unlink($name);
+
+        new JSONStorageEngine($name);
+    }
+}
