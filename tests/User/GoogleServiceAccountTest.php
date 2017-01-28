5c9d1aaa52439235566682191f7e1c782886fac7
Hayden Pierce <hayden@haydenpierce.com>
2017-01-28 06:52:35 -0600

    Fixed incorrect namespace.

--- tests/User/GoogleServiceAccountTest.php
+++ tests/User/GoogleServiceAccountTest.php
@@ -5,7 +5,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace Backup\Tests\Util;
+namespace Backup\Tests\User;
 
 use Backup\User\GoogleServiceAccountUser;
 use PHPUnit\Framework\TestCase;
--- tests/UserBuilder/GoogleServiceAccountUserBuilderTest.php
+++ tests/UserBuilder/GoogleServiceAccountUserBuilderTest.php
@@ -5,7 +5,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace Backup\Tests\Util;
+namespace Backup\Tests\UserBuilder;
 
 use Backup\User\GoogleServiceAccountUser;
 use Backup\UserBuilder\GoogleServiceAccountUserBuilder;
